4e9821a966c204ab6d5f06139f00447772dfbe8c
choleric <nikitafilippov007@gmail.com>
2026-06-06 09:47:30 +0700

    готово

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.pz20belyaevvadim"
+    namespace = "com.example.PR20-pr-23.103-fn"
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.pz20belyaevvadim"
+        applicationId = "com.example.PR20-pr-23.103-fn"
         minSdk = 31
         targetSdk = 36
         versionCode = 1
